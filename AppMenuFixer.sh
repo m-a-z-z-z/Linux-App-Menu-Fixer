1628724990a0f84cc930042017039857a654fc73
Dylan Maziarek <21125921+m-a-z-z-z@users.noreply.github.com>
2024-01-30 01:33:05 +0000

    Code refactored, GNOME case working

--- AppMenuFixer.sh
+++ AppMenuFixer.sh
@@ -1,79 +1,132 @@
 #!/bin/bash
 
+# TODO
+# [] Split program into two cases while testing
+    # [] Hide gnome apps case
+    # [] Hide KDE apps case 
+# [] Hide all org.gnome or org.kde files by default.
+# [] Suspected Gnome or KDE apps made optional / to be selected by user
+# [x] Add programs to be hidden to a text file rather than an array.
+    # This will allow querying by category which returns false positives that we can then blacklist from being hidden.
+    # This will allow for more apps to be hidden besides the regular "org.gnome" or "org.kde" ones.
+# [] Trim whitespace in text file before modifying the .desktop files
+
 # Variable declarations
-log_file="/home/maz/Desktop/HideDElogs/logfile.txt"
-directory="/home/maz/Desktop/Test_desktop_files/"
-gnome_app_array=()
-kde_app_array=()
+logfile="logfile.txt"
+gnome_apps_list="gnome_apps_list.txt"
+kde_apps_list="kde_apps_list.txt"
+modified_files="modifiedFilesList.txt"
+> "$logfile"    # wipe previous log
+> "$gnome_apps_list"
+> "$kde_apps_list"
+directory="/home/maz/Desktop/usr.share.clone/applications"
 
 echo "========================================================================================="
-echo -e "Script will hide KDE apps from Gnome and Gnome apps from KDE to fix the issue of mutliple apps serving the same purpose appearing in the app menu. \n Do you wish to proceed? (yes/no)"
-read answer
+######################## Check if directory is valid ########################
+if [ -d "$directory" ]; then
+    echo "$directory found. Proceeding..." >> $logfile
+else
+    echo -e "\t\tError, Directory not found: $directory\nPlease open script and reconfigure to where your desktop entries are located."
+    echo "Error, Directory not found: $directory. Please open script and reconfigure to where your desktop entries are located." >> $logfile
+    exit 1
+fi
 
-exec >> "$log_file" 2>&1
+######################## Prompt user to proceed ############################
+echo -e "Script will hide KDE apps from GNOME and GNOME apps from KDE to fix the issue of mutliple apps serving the same purpose appearing in the app menu.\nDo you wish to proceed?"
+read -p  "(yes/no): " answer
 
-case "$answer" in 
-    "yes")
-        # Check if directory exists
-        if [ -d "$directory" ]; then
-        
-            # Find Gnome apps and add them to array for sorting through
-            echo -e "\n\tSelect applications to hide:"
-            for gnome_app in "$directory"/Gnome_apps/*org.gnome*; do
-                if [ -e "$gnome_app" ]; then
-                    # echo -e "\t\t$gnome_app"
-                    gnome_app_array+=("$gnome_app")
-                else
-                    echo -e "\t\tNo org.gnome .desktop files found in $directory"
-                fi
-            done
+while [ "$answer" != "yes" ] && [ "$answer" != "y" ] && [ "$answer" != "no" ] && [ "$answer" != "n" ]; do
+    echo "Invalid response, try again."
+    read -p "(yes/no): " answer
+done
 
-            # Sort through apps that need to be edited and apps that can be left alone.
-            echo "Contents of gnome array"
-            for gnome_app in "${gnome_app_array[@]}"; do
-                if grep -qF "NoDisplay=" "$directory"/Gnome_apps/"$gnome_app"; then
-                    echo "$gnome_app is not a menu item application. Already hidden."
-                elif grep -qF "OnlyShowIn=" "$directory"/Gnome_apps/"$gnome_app"; then
-                    sed -i '/OnlyShowIn=/d' "$directory"/Gnome_apps/"$gnome_app" # Delete any current values to avoid complications
-                    echo "Deleting current OnlyShowIn value from $gnome_app"
-                    sed -i '/^\[Desktop Entry\]/a OnlyShowIn=Gnome;' "$directory"/Gnome_apps/"$gnome_app"
-                elif grep -qF "NotShowIn=" "$directory"/Gnome_apps/"$gnome_app"; then
-                    sed -i '/NotShowIn=/d' "$directory"/Gnome_apps/"$gnome_app" # Delete any current values to avoid complications
-                    echo "Deleting current NotShowIn value from $gnome_app"
-                    sed -i '/^\[Desktop Entry\]/a OnlyShowIn=Gnome;' "$directory"/Gnome_apps/"$gnome_app"
-                else
-                    sed -i '/^\[Desktop Entry\]/a OnlyShowIn=Gnome;' "$directory"/Gnome_apps/"$gnome_app"
-                    echo "Appending OnlyShowIn=Gnome; to $gnome_app"
-                fi
-            done
+if [ $answer = "yes" ] || [ $answer = "y" ]; then
+    echo "Proceeding..."
+elif [ $answer = "no" ] || [ $answer = "n" ]; then
+    echo "Operation cancelled. Exiting..."
+    exit 0
+fi
+
+######################## Prompt user to select desktop apps to hide ########################
+echo "\nDo you wish to hide GNOME or KDE apps from the opposing desktop?" 
+read -p "(GNOME = g / KDE = k): " answer
+
+while [ "$answer" != "g" ] && [ "$answer" != "k" ]; do
+    echo "Invalid response, try again."
+    read -p "(GNOME = g / KDE = k): " answer
+done
+
+######################## Find GNOME apps and add them to list for sorting later ########################
+if [ $answer = "g" ]; then
+    echo -e "\n\t\tFinding all GNOME applications..."
+    for app in "$directory"/*.desktop; do
+        if grep -qiF "NoDisplay=true" $app; then     # Check if app contains NoDisplay=true so it can be skipped.
+            echo "$(basename "$app") 'NoDisplay=true'. No need to configure. Skipped." >> $logfile
+
+        elif [[ "$(basename "$app")" == org.gnome* ]]; then     # Add all org.gnome entries to list as they are likely preinstalled with Gnome
+            echo "$(basename "$app")" >> $gnome_apps_list
+
+        elif grep -qi "^Categories=.*\bGNOME\b" "$app"; then
+            # Most precise I was able to get this regex, these programs that are added will be commented out. 
+            # If users wish to hide them, they can uncomment them when the script opens the list in VIM.
+            echo "# $(basename "$app")" >> $gnome_apps_list
 
         else
-            echo -e "\tError, Directory not found: $directory"
-            exit 1
-        fi
+            echo "$(basename "$app") not preinstalled GNOME or KDE app. Skipped." >> $logfile    # Triggers for apps user installed/non system apps
 
-        echo -e "\n\n\t\tApplications hidden successfully. Script will now close."        
+        fi
+    done
+
+    if [ -e "$gnome_apps_list" ]; then
         
-        # Countdown loop
-        for ((i=3; i>0; i--)); do
+        echo -e "\n\tUncomment applications you want hidden by removing '#' at the start of the line."
+        vim "$gnome_apps_list"
+        echo -e "\n\tApps will now be hidden. \n\n\tPress CTRL+C to cancel."
+        for ((i=5; i>0; i--)); do
             echo "$i..."
             sleep 1
         done
-        exec >> /dev/tty 2>&1
-        echo "========================================================================================="
-        exit 0
-        ;;
-    "no")
-        echo -e "\nOperation cancelled. Exiting..."
-        exec >> /dev/tty 2>&1
-        echo "========================================================================================="
-        exit 0
-        ;;
-    *)
-        echo -e "\nInvalid choice. Please enter 'yes' or 'no'."
-        exec >> /dev/tty 2>&1
-        echo "========================================================================================="
-        ;;
-esac
+
+        while IFS= read -r line; do
+            echo -e "\n$line"
+
+            if grep "^#" <<< "$line"; then  # Check if application is commented out from list and skip if it is
+                echo -e "\tPreserving current properties of $line. Skipped." 
+                echo "Preserve current properties of $line. Skipped." >> $logfile
+
+            elif grep -qF "OnlyShowIn=" "$directory/$line"; then
+                echo -e "\tDeleting current 'OnlyShowIn' values from $line."
+                echo "Deleting current 'OnlyShowIn' values from $line." >> $logfile
+                sed -i '/OnlyShowIn=/d' "$directory/$line" # Delete any current values to avoid complications
+                
+                echo -e "\tAppending OnlyShowIn=GNOME to $line"
+                echo "Appending OnlyShowIn=GNOME to $line" >> $logfile
+                sed -i '/^\[Desktop Entry\]/a OnlyShowIn=GNOME;' "$directory/$line"
+
+            elif grep -qF "NotShowIn=" "$directory/$line"; then
+                echo -e "\tDeleting current 'NotShowIn' values from $line."
+                echo "Deleting current 'NotShowIn' values from $line." >> $logfile
+                sed -i '/NotShowIn=/d' "$directory/$line" # Delete any current values to avoid complications
+                
+                echo -e "\tAppending OnlyShowIn=GNOME to $line"
+                echo "Appending OnlyShowIn=GNOME to $line" >> $logfile
+                sed -i '/^\[Desktop Entry\]/a OnlyShowIn=GNOME;' "$directory/$line"
+
+            else
+                echo -e "\tAppending OnlyShowIn=GNOME to $line"
+                echo "Appending OnlyShowIn=GNOME to $line" >> $logfile
+                sed -i '/^\[Desktop Entry\]/a OnlyShowIn=GNOME;' "$directory/$line"
+
+            fi
+        done < "$gnome_apps_list"
+    else
+        echo "Oopsy woopsy, I done messed up somewhere son."
+    fi
+
+elif [ $answer = "k" ]; then
+    echo "To be implemented"
+fi
+
+echo -e "\n\t\tApplications hidden successfully. Exiting script..."
 
 
