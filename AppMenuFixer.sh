d651d058e136da77912b9c3a7c816f4dbc736b86
Dylan Maziarek <21125921+m-a-z-z-z@users.noreply.github.com>
2024-02-02 01:46:18 +0000

    Removed old unused variables. Modified files list created when ran

--- AppMenuFixer.sh
+++ AppMenuFixer.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-# TODO
-# [x] Split program into two cases while testing
-    # [x] Hide gnome apps case
-    # [x] Hide KDE apps case 
-# [] Merge cases once tested
-# [x] Hide all org.gnome apps by default
-# [x] Hide all org.kde apps by default
-# [x] Suspected Gnome or KDE apps made optional / to be selected by user
-# [x] Add programs to be hidden to a text file rather than an array.
-    # This will allow querying by category which returns false positives that we can then blacklist from being hidden.
-    # This will allow for more apps to be hidden besides the regular "org.gnome" or "org.kde" ones.
-# [x] Trim whitespace in text file before modifying the .desktop files
-
 # Variable declarations
 logfile="logfile.txt"
-gnome_apps_list="gnome_apps_list.txt"
-kde_apps_list="kde_apps_list.txt"
 apps_list="apps_list.txt"
 modified_files="modifiedFilesList.txt"
 > "$logfile"    # wipe previous log
-> "$gnome_apps_list"
-> "$kde_apps_list"
 > "$apps_list"
 directory="/home/maz/Desktop/usr.share.clone/applications"
 
@@ -66,6 +49,7 @@ for app in "$directory"/*.desktop; do
     # Skip applications not displayed in app menu and 
     if grep -qiF "NoDisplay=true" "$app" || grep -qiF "NotShowIn=.*\bGNOME\b" "$app" || grep -qiF "NotShowIn=.*\bKDE\b" "$app"; then
         echo "$(basename "$app") Not displayed by default. No need to configure. Skipped." >> "$logfile"
+
 
     elif [ "$answer" = "g" ] && \
     { [[ "$(basename "$app")" == org.gnome* ]] || grep -qi "^Categories=.*\bGNOME\b" "$app"; }; then
@@ -113,25 +97,25 @@ while IFS= read -r line; do
 
     elif grep -qF "OnlyShowIn=" "$directory/$line"; then
         # Remove existing values to avoid any complications
-        echo -e "\tDeleting current 'OnlyShowIn' values from $line." | tee -a $logfile
+        echo -e "\tDeleting current 'OnlyShowIn' values from $line." | tee -a $logfile 
         sed -i '/OnlyShowIn=/d' "$directory/$line"
 
         if [[ "$line" == org.gnome* ]] || grep -qi "^Categories=.*\bGNOME\b" "$directory/$line"; then
-            echo -e "\tAdding OnlyShowIn=GNOME to $line" | tee -a $logfile
+            echo -e "\tAdding OnlyShowIn=GNOME to $line" | tee -a "$logfile" "$modified_files"
             sed -i '/^\[Desktop Entry\]/a OnlyShowIn=GNOME;' "$directory/$line"
 
         elif [[ "$line" == org.kde* ]] || grep -qi "^Categories=.*\bKDE\b" "$directory/$line"; then
-            echo -e "\tAdding OnlyShowIn=KDE to $line" | tee -a $logfile
+            echo -e "\tAdding OnlyShowIn=KDE to $line" | tee -a "$logfile" "$modified_files" 
             sed -i '/^\[Desktop Entry\]/a OnlyShowIn=KDE;' "$directory/$line"
         fi
 
     else
         if [[ "$line" == org.gnome* ]] || grep -qi "^Categories=.*\bGNOME\b" "$directory/$line"; then
-            echo -e "\tAdding OnlyShowIn=GNOME to $line" | tee -a $logfile
+            echo -e "\tAdding OnlyShowIn=GNOME to $line" | tee -a "$logfile" "$modified_files"
             sed -i '/^\[Desktop Entry\]/a OnlyShowIn=GNOME;' "$directory/$line"
 
         elif [[ "$line" == org.kde* ]] || grep -qi "^Categories=.*\bKDE\b" "$directory/$line"; then
-            echo -e "\tAdding OnlyShowIn=KDE to $line" | tee -a $logfile
+            echo -e "\tAdding OnlyShowIn=KDE to $line" | tee -a "$logfile" "$modified_files"
             sed -i '/^\[Desktop Entry\]/a OnlyShowIn=KDE;' "$directory/$line"
         fi
 
